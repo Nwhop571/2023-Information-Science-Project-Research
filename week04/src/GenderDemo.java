9442c2dff016a4cbaa76b01c63decc03cdcabe96
Vihop571 <xrtgh7258@gmail.com>
2023-03-28 19:48:49 +0900

    week04 switch v0.1

---
+++ week04/src/GenderDemo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class GenderDemo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input ssn number(13 digits) : ");
+        String socialSecurityNumber = scanner.nextLine();
+
+        char gender = socialSecurityNumber.charAt(6);
+
+        switch (gender){
+            case '1':
+            case '3':
+                System.out.println("Male");
+                break;
+            case '2':
+            case '4':
+                System.out.println("Female");
+                break;
+        }
+    }
+}
