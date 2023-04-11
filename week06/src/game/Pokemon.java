228353f918c24780fc29fa2f79bdeba501db134d
Vihop571 <xrtgh7258@gmail.com>
2023-04-11 19:42:30 +0900

    week06 class & object v0.2

--- week06/src/game/Pokemon.java
+++ week06/src/game/Pokemon.java
@@ -1,13 +1,43 @@
 package game;
 
 public class Pokemon {
-    String name;
-    int level;
-    int hp;
+    // encapsulation
+
+
+    // fields
+    private String name;
+    private int level;
+    private int hp;
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // methods
     void evolve(){
         level = level + 1;
         hp = hp + 10;
-        System.out.println(name + level + "레벨로 성장 합니다");
+        System.out.println(name + "이(가) " + level + "레벨로 성장 합니다");
     }
     void attack(){
         System.out.println("기본 공격을 시전합니다");
--- week06/src/game/PokemonGame.java
+++ week06/src/game/PokemonGame.java
@@ -5,11 +5,10 @@ public class PokemonGame {
         Pokemon pikachu = new Pokemon();
         Pokemon charizard = new Pokemon();
 
-        pikachu.name = "피카츄";
-        charizard.name = "리자몽";
-
-        pikachu.level = 5;
-        charizard.level = 36;
+        pikachu.setName("피카츄");
+        charizard.setName("리자몽");
+        pikachu.setLevel(5);
+        charizard.setLevel(36);
 
         charizard.evolve();
         charizard.attack();
